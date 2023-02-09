c256dbc689abe1ec807d38ba25d640b39929b256
Alden Cobb <alden.a.cobb@gmail.com>
2023-02-09 14:25:47 -0500

    step 2 complete! .

--- Early JS Projects/Number Guesser/script.js
+++ Early JS Projects/Number Guesser/script.js
@@ -6,3 +6,12 @@ let generateTarget = () => {
     return Math.floor(Math.random() * 9);
 }
 
+let compareGuesses = (playerGuess, computerGuess, target) => {
+    if ((Math.abs(target - playerGuess)) < (Math.abs(target - computerGuess))) {
+        return true;
+    } else if ((Math.abs(target - playerGuess)) > (Math.abs(target - computerGuess))) {
+        return false;
+    } else {
+        return 'Error!';
+    }
+}
